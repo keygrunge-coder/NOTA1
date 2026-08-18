c64eafd2c9caedd8d45bc6a7fa0b6ce69b7df572
rckwd <key.grunge@gmail.com>
2026-08-18 23:03:12 +0700

    UPDATE MANIFEST

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-const cacheName = 'unyil-v1';
-const filesToCache = ['index.html', 'manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('srf-nota-v1').then((cache) => {
+            return cache.addAll([
+                'index.html',
+                'unyil.html',
+                'aldi.html',
+                'leora.html',
+                'manifest.json'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
